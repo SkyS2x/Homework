0f06c1678ae1fac656dabdac09ca5febdc587fda
Spase Zhupanoski <114064525+SkyS2x@users.noreply.github.com>
2024-12-08 13:42:14 +0100

    Price of 30 phones
    Basic JavaScript HomeWorkNo1

---
+++ Price_of_30_phones.js
@@ -0,0 +1,11 @@
+let PhonePrice = 119.95;
+let TaxRate = PhonePrice * 0.05;
+
+let sum = (PhonePrice + TaxRate) * 30;
+
+console.log(TaxRate);
+console.log(sum);
+
+console.log(
+  `The TaxReturn for One phone is ${TaxRate} and the price for 30 phones is ${sum} USD`
+);
