3da6aedce20f3af27242c5d3a48b12d53089bab9
Spase Zhupanoski <114064525+SkyS2x@users.noreply.github.com>
2024-12-09 20:25:48 +0100

    Chinese zodiac
    i did it with if else and switch

---
+++ ChineseZodiac.js
@@ -0,0 +1,90 @@
+// let BirthYear = prompt("What year you are born?");
+// let Years = parseInt(BirthYear);
+// let ChineseZodiac = (Years - 4) % 12;
+
+// if (ChineseZodiac == 0) {
+//     alert("You are Rat");
+// } else if (ChineseZodiac == 1) {
+//     alert("You are Ox");
+// }else if (ChineseZodiac == 2) {
+//     alert("You are Tiger");
+// }else if (ChineseZodiac == 3) {
+//     alert("You are Rabbit");
+// }else if (ChineseZodiac == 4) {
+//     alert("You are Dragon");
+// }else if (ChineseZodiac == 5) {
+//     alert("You are Snake");
+// }else if (ChineseZodiac == 6) {
+//     alert("You are Horse");
+// }else if (ChineseZodiac == 7) {
+//     alert("You are Goat");
+// }else if (ChineseZodiac == 8) {
+//     alert("You are Monkey");
+// }else if (ChineseZodiac == 9) {
+//     alert("You are Rooster");
+// }else if (ChineseZodiac == 10) {
+//     alert("You are Dog");
+// }else if (ChineseZodiac == 11) {
+//     alert("You are Pig");
+// } else {
+//     alert("Are you even Born?");
+// }
+
+
+// let BirthYear = prompt("What year you are born?");
+// let intYears = parseInt(BirthYear);
+// let ChineseZodiac = (intYears - 4) % 12;
+
+// switch(ChineseZodiac){
+//     case 0:
+//         alert("You are Rat");
+//         break;
+//     case 1:
+//         alert("You are Ox");
+//         break;
+//     case 2:
+//         alert("You are Tiger");
+//         break;
+//     case 3:
+//         alert("You are Rabbit");
+//         break;
+//     case 4:
+//         alert("You are Dragon");
+//         break;
+//     case 5:
+//         alert("You are Snake");
+//         break;
+//     case 6:
+//         alert("You are Horse");
+//         break;
+//     case 7:
+//         alert("You are Goat");
+//         break;
+//     case 8:
+//         alert("You are Monkey");
+//         break;
+//     case 9:
+//         alert("You are Roster");
+//         break;
+//     case 10:
+//         alert("You are Dog");
+//         break;
+//     case 11:
+//         alert("You are Pig");
+//         break;
+//     default:
+//         alert("are you alive?");
+// }
+
+
+let promenliva = prompt("What year you are born?");
+// let promenliva = "tocaci 3ca"
+
+switch(promenliva) {
+    case "tocaci 2ca":
+        alert("ja sum kaj tocaci 2ca")
+        break;
+    case "tocaci 3ca":
+        alert("ja sum kaj tocaci 3ca")
+        break;
+}
